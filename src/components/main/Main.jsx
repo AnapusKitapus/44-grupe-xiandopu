8aef31d866d3bfa0ee4583357dd2562541672983
EdvardasNavickas <nps.anapus@gmail.com>
2023-07-28 13:48:37 +0300

    Main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,8 +3,32 @@ import {Column} from './Column';
 
 export function Main() {
     return (
-        <main>
-            MAIN CONTENT
+        <main className={style.main}>
+            <div>
+                Well begun is half done
+            </div>
+            <div>
+                <div>
+                Todays data
+                    <a href='/'>More</a>
+                    <div>
+                        <Column></Column>
+                        <Column></Column>
+                        <Column></Column>
+                        <Column></Column>
+                    </div>
+                </div>
+                <div>
+                    Operations Assistant
+                    <a href='/'>More</a>
+                    <div>
+                        <Column></Column>
+                        <Column></Column>
+                        <Column></Column>
+                        <Column></Column>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
